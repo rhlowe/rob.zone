219593ce576250d25a533af95bc04c4fe5f528ac
Rob <rhlowe@gmail.com>
2020-02-26 21:17:50 -0600

    Add nav, fix colors, make lighthouse more happy

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,18 @@
       </section>
     </header>
 
+    <nav id="navigation" class="bg-v t-w">
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#social">Social</a></li>
+        <li><a href="#tech">❤️ Tech</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
     <main>
-      <article id="intro" class="bg-v t-w">
+      <article id="intro" class="bg-p t-w">
         <section>
           <picture>
             <source srcset="photos/minified/headshot.jpg" media="(min-width: 363px)" />
@@ -33,51 +43,51 @@
           <ul class="flex-list">
             <li>
               <a href="https://www.linkedin.com/in/roberthenrylowe/">
-                <img src="social/minified/linkedin.svg" alt="LinkedIn" class="svg-img" />
+                <img src="social/minified/linkedin.svg" alt="LinkedIn Logo" class="svg-img" />
                 <span>in/roberthenrylowe</span>
               </a>
             </li>
 
             <li>
               <a href="https://twitter.com/rhlowe">
-                <img src="social/minified/twitter.svg" alt="Twitter" class="svg-img" />
+                <img src="social/minified/twitter.svg" alt="Twitter Logo" class="svg-img" />
                 <span>@rhlowe</span>
               </a>
             </li>
 
             <li>
               <a href="https://github.com/rhlowe">
-                <img src="social/minified/github.svg" alt="Github" class="svg-img" />
+                <img src="social/minified/github.svg" alt="Github Logo" class="svg-img" />
                 <span>rhlowe</span>
               </a>
             </li>
 
             <li>
               <a href="mailto:rhlowe@gmail.com">
-                <img src="social/minified/email.svg" alt="Email" class="svg-img" />
+                <img src="social/minified/email.svg" alt="Email Logo" class="svg-img" />
                 <span>rhlowe@gmail.com</span>
               </a>
             </li>
           </ul>
 
           <p class="ta-c">
-            <em>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></em>
+            <em>Icons provided by <a href="https://www.flaticon.com/authors/freepik" title="Freepik's author page on www.flaticon.com">Freepik</a> and <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect's author page on www.flaticon.com">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></em>
           </p>
         </section>
       </article>
 
-      <article id="skills">
+      <article id="tech">
         <section>
-          <h2>Skills</h2>
+          <h2>Tech I ❤️ Right now</h2>
 
           <ul>
+            <li><img src="logos/minified/a11y-logo-hero.svg" alt="Accessibility" loading="lazy" /></li>
             <li><img src="logos/minified/HTML5_logo_and_wordmark.svg" alt="HTML5 logo and wordmark" loading="lazy" /></li>
             <li><img src="logos/minified/CSS3_logo_and_wordmark.svg" alt="CSS3 logo and wordmark" loading="lazy" /></li>
             <li><img src="logos/minified/JavaScript-logo.png" alt="JavaScript logo" loading="lazy" /></li>
             <li><img src="logos/minified/ember_Tomster.png" alt="Ember Tomster" loading="lazy" /></li>
             <li><img src="logos/minified/vuejs.svg" alt="Vue logo" loading="lazy" /></li>
             <li><img src="logos/minified/React_logo.png" alt="React" loading="lazy" /></li>
-            <li><img src="logos/minified/a11y-logo-hero.svg" alt="Accessibility" loading="lazy" /></li>
           </ul>
         </section>
       </article>
@@ -85,6 +95,18 @@
       <article id="resume">
         <section>
           <h2>Resume</h2>
+
+          <h3>Work Experience</h3>
+
+          <h3>Accomplishments</h3>
+
+          <ul>
+            <li>2020: Q2 Circle of Awesomeness - Culture Champion of the Year Finalist</li>
+          </ul>
+
+          <h3>Certifications</h3>
+
+          <h3>Education</h3>
         </section>
       </article>
 
@@ -95,7 +117,7 @@
       </article>
     </main>
 
-    <footer>
+    <footer class="bg-p t-w">
       <section>
         <p class="ta-c">Copyright © Me, Forever.</p>
       </section>
