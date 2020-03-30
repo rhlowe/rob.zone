dd3b8b7624705311c7262bee4d1b957e2c6b99d2
Rob <rhlowe@gmail.com>
2020-03-30 09:18:01 -0500

    Update favicon.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
    */
   head: {
     htmlAttrs: {
-      lang: 'en-US'
+      lang: 'en-US',
     },
     title: process.env.npm_package_name || '',
     meta: [
@@ -14,10 +14,16 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.svg',
+      },
+    ],
   },
   /*
    ** Customize the progress-bar color
@@ -38,7 +44,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
   ],
   /*
    ** Nuxt.js modules
@@ -51,6 +57,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
-}
+    extend(config, ctx) {},
+  },
+};
